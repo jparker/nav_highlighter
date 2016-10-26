81d0780fb14d3730255f02101c134b7bd496e756
John Parker <jparker@urgetopunt.com>
2016-10-25 21:11:42 -0700

    still trying to fix the autoloading issues

--- app/helpers/nav_highlighter/application_helper.rb
+++ app/helpers/nav_highlighter/application_helper.rb
@@ -1,10 +1,8 @@
-require 'nav_highlighter/utils'
-
 module NavHighlighter
   module ApplicationHelper
     def nav_item(content = nil, name: nil, **options, &block)
       active = @_current_nav == name ? nav_highlight_class : nil
-      options[:class] = Utils.merge_classes active, options[:class]
+      options[:class] = NavHighlighter::Utils.merge_classes active, options[:class]
       content_tag :li, content, **options, &block
     end
 
--- lib/nav_highlighter.rb
+++ lib/nav_highlighter.rb
@@ -1,6 +1,7 @@
 require 'nav_highlighter/engine'
 require 'nav_highlighter/configuration'
 require 'nav_highlighter/nav_highlighting'
+require 'nav_highlighter/utils'
 
 module NavHighlighter
 end
