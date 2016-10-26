2f59e850fd2759482a7cfa2ac4d040cfa8ec9cf8
John Parker <jparker@urgetopunt.com>
2016-10-25 20:59:13 -0700

    trying to work around rails autoloading issues
    In development, after making changes which trigger Rails to reload
    classes, if I hit a page after the app reloads, an ArgumentError gets
    raised with the following message:
    
    	A copy of NavHighlighter::ApplicationHelper has been removed
    	from the module tree but is still active!
    
    I'm not sure what's causing this, but I suspect it's an issue with Rails
    autoloading. I'm moving NavHighlighter::NavHilighting into its own file
    to reflect what Rails autoloading is expecting in the hopes that this
    will fix the problem.

--- lib/nav_highlighter.rb
+++ lib/nav_highlighter.rb
@@ -1,13 +1,6 @@
 require 'nav_highlighter/engine'
 require 'nav_highlighter/configuration'
+require 'nav_highlighter/nav_highlighting'
 
 module NavHighlighter
-  module NavHighlighting
-    def nav_highlight(name = controller_name.to_sym, **options, &block)
-      before_action **options do
-        name = instance_eval(&block) if block_given?
-        @_current_nav = name
-      end
-    end
-  end
 end
---
+++ lib/nav_highlighter/nav_highlighting.rb
@@ -0,0 +1,10 @@
+module NavHighlighter
+  module NavHighlighting
+    def nav_highlight(name = controller_name.to_sym, **options, &block)
+      before_action **options do
+        name = instance_eval(&block) if block_given?
+        @_current_nav = name
+      end
+    end
+  end
+end
