cea1e8f69f177d6f4b1c34db7d7c3805bf0e386a
John Parker <jparker@urgetopunt.com>
2016-12-23 08:31:48 -0800

    fixed initializer name

--- lib/nav_highlighter/engine.rb
+++ lib/nav_highlighter/engine.rb
@@ -2,7 +2,7 @@ module NavHighlighter
   class Engine < ::Rails::Engine
     isolate_namespace NavHighlighter
 
-    initializer 'comparison.view_helpers' do
+    initializer 'nav_highlighter.view_helpers' do
       ActionView::Base.send :include, helpers
     end
   end
